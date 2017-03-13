4e5f614f1b30650929e70d02cd13c7699e14e7c6
Katchapon <Yowa@Katchapons-MacBook-Pro.local>
2017-03-13 16:35:53 +0700

    implement intro view controller

---
+++ DITPDrive/ApplicationStoryboard.swift
@@ -0,0 +1,21 @@
+//
+//  ApplicationStoryboard.swift
+//  DITPDrive
+//
+//  Created by Katchapon Poolpipat on 3/13/2560 BE.
+//  Copyright © 2560 Katchapon Poolpipat. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIStoryboard {
+  
+  class func mainStoryboard() -> UIStoryboard {
+    return UIStoryboard(name: "Main", bundle: nil)
+  }
+  
+  class func homeStoryboard() -> UIStoryboard {
+    return UIStoryboard(name: "Home", bundle: nil)
+  }
+}
--- DITPDrive/IntroPageViewController.swift
+++ DITPDrive/IntroPageViewController.swift
@@ -8,37 +8,88 @@
 
 import UIKit
 
+
 class IntroPageViewController: UIPageViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
+  
+  fileprivate lazy var introStepViewControllers: [UIViewController] = {
+    return [self.introStepViewController(withIdentifier: "step1"),
+            self.introStepViewController(withIdentifier: "step2"),
+            self.introStepViewController(withIdentifier: "step3")]
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    setupIntroStep()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+  
+  fileprivate func introStepViewController(withIdentifier identifier: String) -> UIViewController {
+    return UIStoryboard.mainStoryboard().instantiateViewController(withIdentifier: identifier)
+  }
+  
+  fileprivate func setupIntroStep() {
+    
+    dataSource = self
+    
+    setViewControllers([introStepViewControllers.first!], direction: .forward, animated: true, completion: nil)
+  }
+  
 }
 
 extension IntroPageViewController: UIPageViewControllerDataSource {
   
+  func presentationCount(for pageViewController: UIPageViewController) -> Int {
+    return introStepViewControllers.count
+  }
+  
+  func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+    guard let firstViewController = viewControllers?.first,
+     let firstViewControllerIndex = introStepViewControllers.index(of: firstViewController) else {
+      return 0
+    }
+    
+    return firstViewControllerIndex
+  }
+  
   func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-    return nil
+    
+    guard let viewControllerIndex = introStepViewControllers.index(of: viewController) else {
+      return nil
+    }
+    
+    let previousIndex = viewControllerIndex - 1
+    
+    guard previousIndex >= 0  else {
+      return nil
+    }
+    
+    guard introStepViewControllers.count > previousIndex else {
+      return nil
+    }
+    
+    return introStepViewControllers[previousIndex]
   }
   
   func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-    return nil
+    
+    guard let viewControllerIndex = introStepViewControllers.index(of: viewController) else {
+      return nil
+    }
+    
+    let nextIndex = viewControllerIndex + 1
+    
+    guard introStepViewControllers.count != nextIndex else {
+      return nil
+    }
+    
+    guard introStepViewControllers.count > nextIndex else {
+      return nil
+    }
+    
+    return introStepViewControllers[nextIndex]
   }
 }
