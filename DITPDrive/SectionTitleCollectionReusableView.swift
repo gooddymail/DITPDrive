b2e2e696227a08e1b6f06522b1808523edb3c76d
Katchapon <Yowa@Katchapons-MacBook-Pro.local>
2017-03-29 10:26:51 +0700

    add collectionView header

---
+++ DITPDrive/SectionTitleCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  SectionTitleCollectionReusableView.swift
+//  DITPDrive
+//
+//  Created by Katchapon Poolpipat on 3/27/2560 BE.
+//  Copyright © 2560 Katchapon Poolpipat. All rights reserved.
+//
+
+import UIKit
+
+class SectionTitleCollectionReusableView: UICollectionReusableView {
+        
+}
